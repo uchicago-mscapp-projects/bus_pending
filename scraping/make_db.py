55d8d84f2639d8e790638de393f6c82fa188e560
Michael Rosenbaum <michaelr526@gmail.com>
2024-02-16 15:44:30 -0600

    Update to include downloads from CTA site, WIP

--- scraping/make_db.py
+++ scraping/make_db.py
@@ -18,15 +18,42 @@ SCHEMA_BUSES = '''
         tablockid TEXT,
         zone TEXT,
         origtatripno INTEGER,
-        PRIMARY KEY (vid, tmstmp)
-       );
+        PRIMARY KEY (vid, tmstmp),
+        FOREIGN KEY (rt) REFERENCES schedule(rt)
+    );
 '''
-# TODO: Add Scheduling table
-# TODO: Add FOREIGN_KEYS
-# TODO: Add indexing
 
-SCHEMA_TIMES = '''
+SCHEMA_STOPS = '''
+    CREATE TABLE stops (
+        stop_id INT NOT NULL,
+        stop_code INT NOT NULL,
+        stop_name TEXT NOT NULL,
+        stop_desc TEXT,
+        stop_lat REAL NOT NULL,
+        stop_lon REAL NOT NULL,
+        location_type BOOLEAN,
+        parent_station BOOLEAN,
+        wheelchair_boarding BOOLEAN,
+        PRIMARY KEY (stop_desc)
+    );
 '''
+
+# From stop_times
+SCHEMA_SCHEDULE = '''
+    CREATE TABLE schedule (
+        trip_id TEXT NOT NULL, 
+        arrival_time TEXT NOT NULL, 
+        departure_time TEXT NOT NULL,
+        stop_id INT NOT NULL, 
+        stop_sequence INT, # Order of routes 
+        stop_headsign TEXT NOT NULL, # End stattion
+        pickup_type BOOL,
+        shape_dist_traveled TEXT,
+        PRIMARY KEY (trip_id, stop_id, stop_headsign),
+        FOREIGN KEY (stop_id) REFERENCES stops(stop_desc)
+    );
+'''
+
 
 def make_db():
     '''
@@ -40,6 +67,6 @@ def make_db():
     cursor = connection.cursor()
 
     # Create table for both schems
-    for schema in [SCHEMA_BUSES]:
+    for schema in [SCHEMA_BUSES, SCHEMA_SCHEDULE, SCHEMA_STOPS]:
         cursor.execute(schema)
     cursor.close()
