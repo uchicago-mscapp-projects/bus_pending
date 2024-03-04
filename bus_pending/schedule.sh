410e9cfb79ef09dd5a4f3b1e8adcad9e3e54322b
Michael Rosenbaum <michaelr526@gmail.com>
2024-03-03 22:47:54 -0600

    Fixing comment typos

--- bus_pending/schedule.sh
+++ bus_pending/schedule.sh
@@ -3,7 +3,7 @@
 if test -e ./data/buses.db 
 then
     python -m scraping --quiet
-# Passing to y to bash from: https://stackoverflow.com/a/3385064
+# Passing y to bash from: https://stackoverflow.com/a/3385064
 else 
     y | python -m scraping
 fi
