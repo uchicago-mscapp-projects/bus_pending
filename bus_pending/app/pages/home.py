20ba65ac6b9147729d96a62086a9223d398ed1a8
Regina I. Medina <rmedina@uchicago.edu>
2024-03-03 19:06:27 -0600

    Updated .gitignore

--- bus_pending/app/pages/home.py
+++ bus_pending/app/pages/home.py
@@ -4,11 +4,12 @@ from dash import html
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-    html.H3('About this project'),
+    html.H3('Welcome to Bus Pending'),    
     html.P('We are CAPP30122 students! And we wanted to know how late buses are in Chicago.'),
     html.P('Feel free to wonder around this page and see some of what we found.'),
     html.P('Note: If you open a Pydeck plot, return to this home page with the web browser navigator \"back\" button.'),
-
+    
+    html.H3('About this project'),
     html.P('Bus Pending collects and analyzes high-frequency location data of Chicago’s public buses to better understand how often buses are delayed, and for whom delays occur. It combines live bus position data with bus schedules and demographic information on Chicago buses. These data are displayed in a live dashboard. This application also contains resources to schedule and request location data to continue the project. The data linked here represents one week of bus positions, from February 22nd to 28th, 2024.'),
 
     html.H4('Data Sources'), 
