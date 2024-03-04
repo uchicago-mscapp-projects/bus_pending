c85968a43ce22f097f8a557a44807da1c17b4ac2
Michael Rosenbaum <michaelr526@gmail.com>
2024-03-03 21:55:02 -0600

    Updated __main__ for cleaning

--- bus_pending/__main__.py
+++ bus_pending/__main__.py
@@ -1,9 +1,26 @@
+import sys
 import pathlib
-import pandas as pd
-from cleaning.clean import write
 
-filename = pathlib.Path(__file__).parent/"data/your_database"
+import bus_pending.app as app
+import bus_pending.analysis.analyze_real_data as analyze
+import bus_pending.cleaning.clean as clean
+import bus_pending.visualizations.acs as viz_acs
+import bus_pending.visualizations.app as viz_app
+import bus_pending.visualizations.deck_app as viz_deck
+import bus_pending.visualizations.maps as viz_maps
+import bus_pending.visualizations.plotly_maps as viz_plotly
+import bus_pending.visualizations.pydeck_maps as viz_pydeck
+
 
 if __name__ == '__main__':
 
-    write(filename)
+    # If data doesn't exist exit with error
+    db = pathlib.Path(__file__).parents[1] / 'data/buses.db'
+    if not db.exists():
+        raise FileNotFoundError('data/buses.db is needed to run the app.')
+    
+    # First clean and analyze data
+    clean.write(db)
+    clean_data = pathlib.Path(__file__).parents[1] / 'data/trip_time_level.csv'
+    analyze.do_analysis(clean_data)
+    
