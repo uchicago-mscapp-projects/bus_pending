2d8092bf435ee2ee5c5f783131524f4ae06bf4bf
Daniel Munoz <146023389+dmunozbatista@users.noreply.github.com>
2024-03-04 12:45:18 -0600

    Fixing for main

--- bus_pending/analysis/analyze_real_data.py
+++ bus_pending/analysis/analyze_real_data.py
@@ -6,6 +6,9 @@ from typing import Tuple
 import pathlib
 
 
+csv_path_stats = pathlib.Path(__file__).parents[2] / "data/stats_df.csv"
+csv_path_complete = pathlib.Path(__file__).parents[2] / "data/complete_info.csv"
+
 
 def determine_time_data(time_stmp: str) -> Tuple[str, bool]:
     """
