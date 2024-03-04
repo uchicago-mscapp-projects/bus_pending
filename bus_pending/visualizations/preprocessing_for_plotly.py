25a3286188160203f25d16f15d26d6c90fb52b2d
Regina I. Medina <rmedina@uchicago.edu>
2024-03-04 11:39:09 -0600

    Added black and cleaned viz scripts

--- bus_pending/visualizations/preprocessing_for_plotly.py
+++ bus_pending/visualizations/preprocessing_for_plotly.py
@@ -1,26 +1,27 @@
 import plotly.express as px
-import pandas as pd 
+import pandas as pd
 import geopandas as gpd
 import json
 import numpy as np
 from skimpy import clean_columns
 import pathlib
 
+
 def clean_income_data(file, year):
     """
     Takes income data from ACS tables and aggregates it by zipcode in a format
-    that easily readable by plotly. 
+    that easily readable by plotly.
 
-    The desaggregation for the ACS tables is: zip code, income. 
+    The desaggregation for the ACS tables is: zip code, income.
 
-    Args: 
-        - file (str): Common path were the files are stored 
-        - year (int): the year to be processed 
+    Args:
+        - file (str): Common path were the files are stored
+        - year (int): the year to be processed
 
-    Returns (pandas data frame): a data frame with income data by zip code 
-    by year. 
-    """ 
-    # Open file 
+    Returns (pandas data frame): a data frame with income data by zip code
+    by year.
+    """
+    # Open file
     file = open(file + str(year) + ".csv")
     df_income_raw = pd.read_csv(file)
     file.close()
@@ -29,20 +30,21 @@ def clean_income_data(file, year):
     # Clean variable names from income table
     df_income_wide = clean_columns(df_income_raw)
     df_income_wide = df_income_wide.drop(
-        df_income_wide.filter(regex = "margin_").columns, axis = 1)
+        df_income_wide.filter(regex="margin_").columns, axis=1
+    )
 
-    # Select total population 
-    df_income_wide = df_income_wide[df_income_wide["label_grouping"].str.contains("Total")]
+    # Select total population
+    df_income_wide = df_income_wide[
+        df_income_wide["label_grouping"].str.contains("Total")
+    ]
 
-    # Change to long format 
+    # Change to long format
     # Store all column names for zip codes
     zip_cols = df_income_wide.columns[1:1399]
-    df_income = pd.melt(df_income_wide, 
-                        id_vars = "label_grouping", 
-                        value_vars=zip_cols)
+    df_income = pd.melt(df_income_wide, id_vars="label_grouping", value_vars=zip_cols)
 
-    df_income = df_income.rename(columns = {"variable": "zip", "value": "income"})
-    df_income = df_income.drop("label_grouping", axis = 1)
+    df_income = df_income.rename(columns={"variable": "zip", "value": "income"})
+    df_income = df_income.drop("label_grouping", axis=1)
 
     df_income["zip"] = df_income["zip"].str.replace(r"zcta_5_", "")
     df_income["zip"] = df_income["zip"].str.replace(r"_estimate", "")
@@ -51,71 +53,70 @@ def clean_income_data(file, year):
 
     # Replace observations that are entirely white space with NaN
     # Source: https://stackoverflow.com/questions/13445241/replacing-blank-values-white-space-with-nan-in-pandas
-    df_income["income"] = df_income["income"].replace(r"^\s*$", np.nan, regex=True).astype(float)
+    df_income["income"] = (
+        df_income["income"].replace(r"^\s*$", np.nan, regex=True).astype(float)
+    )
     df_income["year"] = year
-    
+
     return df_income
 
 
-def write_income_series(min_year = 2018, max_year = 2022): 
+def write_income_series(min_year=2018, max_year=2022):
     """
-    Build panel data on income by zip code. 
+    Build panel data on income by zip code.
 
-    Args: 
-        - min_year (int): Starting year for the series 
-        - max_year (int): Ending year for the series 
+    Args:
+        - min_year (int): Starting year for the series
+        - max_year (int): Ending year for the series
 
-    Returns: No returns, but instead it writes the clean acs data. 
+    Returns: No returns, but instead it writes the clean acs data.
     """
     df_income_zip_code_series = pd.DataFrame()
 
     for year in range(min_year, max_year + 1):
-        csv_name = '/acsdata/acs_income_zipcodes_' + str(year) + ".csv"
+        csv_name = "/acsdata/acs_income_zipcodes_" + str(year) + ".csv"
         path = pathlib.Path(__file__) / csv_name
 
         df_year = clean_income_data(path, year)
-        
-        df_income_zip_code_series = pd.concat(
-            [df_income_zip_code_series, df_year])
-        
+
+        df_income_zip_code_series = pd.concat([df_income_zip_code_series, df_year])
+
     write_path = pathlib.Path(__file__) / "acs_data/df_income_zip_code_series.csv"
-    df_income_zip_code_series.to_csv(write_path, 
-                                     index = False)
+    df_income_zip_code_series.to_csv(write_path, index=False)
 
 
-
-# Convert shapefiles into geodata 
-def convert_shapefiles(file_location, file_extension): 
+# Convert shapefiles into geodata
+def convert_shapefiles(file_location, file_extension):
     """
-    Import original shape file published by the City of Chicago and transform 
-    into geojson data for plotly package. 
+    Import original shape file published by the City of Chicago and transform
+    into geojson data for plotly package.
 
-    Args: 
-        -   file_location (str): path to access file with .spx extension  
+    Args:
+        -   file_location (str): path to access file with .spx extension
         -   file_extension (str): file extension
     """
     chicago_geodata = gpd.read_file(file_location + file_extension)
-    chicago_geodata.to_file(file_location + ".geojson", 
-                            driver = "GeoJSON")
+    chicago_geodata.to_file(file_location + ".geojson", driver="GeoJSON")
 
-def make_trial_data(geojson, locations): 
+
+def make_trial_data(geojson, locations):
     """
     Takes jsonfile and extracts the list of communities/zip codes.
     Then it adds trial data that holds no meaning
 
-    Args: 
-    - geojson (geojson): shape file in geojson format, 
+    Args:
+    - geojson (geojson): shape file in geojson format,
     - locations (str): name of the variable that has the locations names
-    
+
     """
-    df = pd.DataFrame(geojson, columns = [locations])
+    df = pd.DataFrame(geojson, columns=[locations])
     df["trial_numbers"] = range(50, 50 + len(df))
 
 
 # Build Chicago Neighborhoods map
-def map_neighborhoods(): 
-    
-    # Import shapefiles 
+def map_neighborhoods():
+
+    # Import shapefiles
     file = open("../data/geodata/Boundaries - Community Areas (current).geojson")
     geojson = json.load(file)
     file.close()
@@ -124,22 +125,20 @@ def map_neighborhoods():
 
     fig = px.choropleth(
         # Data frame with variables of interest
-        df, 
-        # Geojson with geography 
-        geojson = geojson, 
+        df,
+        # Geojson with geography
+        geojson=geojson,
         # Variable that is to be mapped as color
-        color = "trial_numbers",
+        color="trial_numbers",
         # Key in geojson to link with data frame
-        featureidkey = "properties.community",
-        locations = "community_name", 
+        featureidkey="properties.community",
+        locations="community_name",
         # Coordinate to center the map around
-        center = dict(lat = 41.8781, lon = -87.6298), 
-        # 
-        projection = "mercator"
+        center=dict(lat=41.8781, lon=-87.6298),
+        #
+        projection="mercator",
     )
 
     # Adjust zoom to display Chicago (based on the shapefile)
     fig.update_geos(fitbounds="locations")
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-    
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
--- bus_pending/visualizations/preprocessing_for_pydeck.py
+++ bus_pending/visualizations/preprocessing_for_pydeck.py
@@ -6,47 +6,48 @@ import json
 
 def make_time_unix(gdf, time_column_name):
     """
-    Takes a pands column with time data and converts it into unix format. 
-    It then standardizes it to start from 0. 
+    Takes a pands column with time data and converts it into unix format.
+    It then standardizes it to start from 0.
 
-    Args: 
-        - df (geopandas data frame): A data frame with a time column 
-        - time_column_name (string): The name of  pandas column with time data 
+    Args:
+        - df (geopandas data frame): A data frame with a time column
+        - time_column_name (string): The name of  pandas column with time data
         stored as a string with "yyyy-mm-dd hh:mm" format.
 
-    Returns (pandas data frame): A pandas data frame with a new column with 
-    time as unix format and 
+    Returns (pandas data frame): A pandas data frame with a new column with
+    time as unix format and
     """
 
-    gdf["unix_tmstmp"] = gdf[time_column_name].astype('datetime64[s]').astype('int')
-    gdf["unix_tmstmp"] = gdf["unix_tmstmp"] - min(gdf["unix_tmstmp"] ) 
-    
+    gdf["unix_tmstmp"] = gdf[time_column_name].astype("datetime64[s]").astype("int")
+    gdf["unix_tmstmp"] = gdf["unix_tmstmp"] - min(gdf["unix_tmstmp"])
+
     return gdf
 
-def transform_points_to_paths(gdf, points_col_name, time_col_name): 
-    """
-    Collapses a series of points observations (stored as a geometry column) 
-    into a single "path" observation. The format complies with pydeck 
-    specifications to plot paths. 
 
-    Args: 
-    - gdf (geopandas data frame): A data frame with trips information, 
-    the unit of analysis is a snaptshot of all buseses at differents points in 
-    time. 
-    - points_col_name (string): The name of a column with geometry data in the 
-    form of points. 
-    - time_col_name (string): The name of column with time data in 
+def transform_points_to_paths(gdf, points_col_name, time_col_name):
+    """
+    Collapses a series of points observations (stored as a geometry column)
+    into a single "path" observation. The format complies with pydeck
+    specifications to plot paths.
+
+    Args:
+    - gdf (geopandas data frame): A data frame with trips information,
+    the unit of analysis is a snaptshot of all buseses at differents points in
+    time.
+    - points_col_name (string): The name of a column with geometry data in the
+    form of points.
+    - time_col_name (string): The name of column with time data in
     unix format.
 
-    Returns (data frame): A pandas data frame with trips data. The unit of 
-    analysis is one observation per complete trip (instead of point in time). 
-    In a format that is readable by pydeck.  
+    Returns (data frame): A pandas data frame with trips data. The unit of
+    analysis is one observation per complete trip (instead of point in time).
+    In a format that is readable by pydeck.
 
     """
     trips = gdf["vid"].unique()
     df_trips_trails = pd.DataFrame()
 
-    for trip in trips: 
+    for trip in trips:
         df_bus = gdf[gdf["vid"] == trip]
         route = df_bus["rt"].unique()[0]
         df_bus = pd.DataFrame(df_bus[[points_col_name, time_col_name]])
@@ -55,45 +56,46 @@ def transform_points_to_paths(gdf, points_col_name, time_col_name):
         list_tmstmp = list(df_bus[time_col_name])
 
         data = [[route, trip, list_coordinates, list_tmstmp]]
-        
-        # Create the pandas DataFrame
-        df_bus_trail = pd.DataFrame(data, columns=['route', 'vid', 'coordinates', 'tmstmp'])
 
-        df_trips_trails = pd.concat([df_trips_trails, df_bus_trail], axis = 0)
-    
+        # Create the pandas DataFrame
+        df_bus_trail = pd.DataFrame(
+            data, columns=["route", "vid", "coordinates", "tmstmp"]
+        )
+
+        df_trips_trails = pd.concat([df_trips_trails, df_bus_trail], axis=0)
+
     return df_trips_trails
 
 
-def clean_bus_trips(): 
-    """ 
-    Takes bus scraped data and turns it into a format that pydeck "PathLayer" 
-    can read. 
+def clean_bus_trips():
+    """
+    Takes bus scraped data and turns it into a format that pydeck "PathLayer"
+    can read.
     """
     # STEP 1. Convert raw scraped data into json
     filename = pathlib.Paht(__file__) / "scraped_data/bus_positions.json"
 
-    file = open(filename) 
+    file = open(filename)
     bus_positions = json.load(file)
     file.close()
 
-    # Collapse data into buses 
+    # Collapse data into buses
     all_buses = []
 
-    for response in bus_positions: 
+    for response in bus_positions:
         if "vehicle" in response["bustime-response"]:
-            for bus in response["bustime-response"]["vehicle"]: 
+            for bus in response["bustime-response"]["vehicle"]:
                 bus["coordinates"] = [bus["lon"], bus["lat"]]
                 all_buses.append(bus)
 
     df_buses = pd.read_json(json.dumps(all_buses))
     df_buses["geometry"] = gpd.points_from_xy(df_buses.lon, df_buses.lat)
-    gdf_buses = gpd.GeoDataFrame(df_buses, geometry='geometry')
+    gdf_buses = gpd.GeoDataFrame(df_buses, geometry="geometry")
 
     gdf_buses_unix_time = make_time_unix(gdf_buses, "tmstmp")
-    df_trails = transform_points_to_paths(gdf_buses_unix_time, "coordinates", "unix_tmstmp")
-
+    df_trails = transform_points_to_paths(
+        gdf_buses_unix_time, "coordinates", "unix_tmstmp"
+    )
 
     write_path = pathlib.Path(__file__) / "geodata/trips_trails.json"
-    df_trails.to_json(write_path, orient = "records")
-    
-
+    df_trails.to_json(write_path, orient="records")
